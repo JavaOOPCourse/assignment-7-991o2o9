92d8eb520d1d3a9b23740b1b01f8c141231debd6
991o2o9 <1kkro123@gmail.com>
2026-03-25 17:18:23 +0600

    done

--- src/StudentRecordProcessor.java
+++ src/StudentRecordProcessor.java
@@ -1,3 +1,7 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class StudentRecordProcessor {
     // Поля для хранения данных
@@ -14,6 +18,36 @@ public class StudentRecordProcessor {
      */
     public void readFile() {
         // TODO: реализуйте чтение файла здесь
+        try (BufferedReader bf = new BufferedReader(new FileReader("input/students.txt"))){
+            String line;
+            while ((line = bf.readLine()) != null){
+                try{
+                    String[] parts = line.split(",");
+
+                    if(parts.length != 2){
+                        throw new InvalidScoreException("Invalid format");
+                    }
+
+                    String name = parts[0];
+                    int score = Integer.parseInt(parts[1]);
+
+                    if (score < 0 || score > 100){
+                        throw new InvalidScoreException("Score out of range");
+                    }
+                    students.add(new Student(name,score));
+                    System.out.println("Valid: " + line);
+                }
+                catch (NumberFormatException | InvalidScoreException e){
+                    System.out.println("Invalid data: " + line);
+                }
+            }
+        }
+        catch (FileNotFoundException e){
+            System.out.println("File is not found");
+        }
+        catch (IOException e){
+            System.out.println("Something went wrong");
+        }
     }
 
     /**
@@ -21,6 +55,23 @@ public class StudentRecordProcessor {
      */
     public void processData() {
         // TODO: обработка данных и сортировка здесь
+        if(students.isEmpty()){
+            return;
+        }
+        int sum = 0;
+        highestStudent = students.get(0);
+
+        for (Student s : students){
+            sum += s.getScore();
+
+            if(s.getScore() > highestStudent.getScore()){
+                highestStudent = s;
+            }
+        }
+
+        averageScore = (double) sum / students.size();
+        Collections.sort(students);
+        System.out.println(students);
     }
 
     /**
@@ -28,6 +79,26 @@ public class StudentRecordProcessor {
      */
     public void writeFile() {
         // TODO: запись результата в файл здесь
+        try(BufferedWriter bw = new BufferedWriter(new FileWriter("output/report.txt"))){
+            if (students.isEmpty()) {
+                System.out.println("No data to write");
+                return;
+            }
+
+            bw.write("Average: " + averageScore);
+            bw.newLine();
+
+            bw.write("Highest: " + highestStudent.getName() + " - " + highestStudent.getScore());
+            bw.newLine();
+
+            for (Student s : students){
+                bw.write(s.getName() + " - " + s.getScore());
+                bw.newLine();
+            }
+        }
+        catch (IOException e){
+            System.out.println("Something went wrong");
+        }
     }
 
     public static void main(String[] args) {
@@ -45,4 +116,41 @@ public class StudentRecordProcessor {
 }
 
 // class InvalidScoreException реализуйте меня
-// class Student (name, score)
+
+class InvalidScoreException extends Exception{
+    public InvalidScoreException(String msg){
+        super(msg);
+    }
+}
+
+class Student implements Comparable<Student>{
+    private String name;
+    private int score;
+
+    public Student(String name,int score){
+        this.name = name;
+        this.score = score;
+    }
+
+    @Override
+    public int compareTo(Student other){
+        return Integer.compare(other.getScore(), score);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", score=" + score +
+                '}';
+    }
+}
+
